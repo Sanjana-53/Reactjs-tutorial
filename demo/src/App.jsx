f6b208d1d55c28645efb6a4b3568a1833ed0a325
Sanjana-53 <sanjanacps185@gmail.com>
2026-08-31 15:54:24 +0530

    sucessfully committed

---
+++ demo/src/App.jsx
@@ -0,0 +1,14 @@
+const App = ()=>{
+  const a=10;
+  return (
+  <>                
+    <h1>Hello World </h1>
+    <h2>Hi Everyone</h2>
+    <h3>{a}</h3>
+  </>
+  );
+}//empty tag in react are called fragment
+export default App
+
+//UI
+//1. Imperative   2. Declarative
