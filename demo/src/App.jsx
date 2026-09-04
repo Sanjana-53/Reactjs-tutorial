512f0aa1de606ac550a57693201688d0108c41cd
Sanjana-53 <sanjanacps185@gmail.com>
2026-09-04 15:17:03 +0530

    update commit

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -2,28 +2,29 @@ import Product from "./Product"
 const App = ()=>{
   const a=10;
   let toggle = false;
-  let prodt;
-  if(toggle){
-
-    prodt = <>
-    <Product name="HP" price={60}/>
-    <Product name="Vivo" price={30}/>
-    <Product name="Acer" price={50}/>
-      </>
-   }
-   else{
-    prodt = <>
-    <Product name="Lenovo" price={75}/>
-    <Product name="Macbook" price={130}/>
-    </>
-   }
+  //let prodt;
+  
   return (
   <>                
     <h1>Hello World </h1>
     <h2>Hi Everyone</h2>
     <h3>{a}</h3>
     <h1>Product page</h1>
-    {prodt}
+    {
+      toggle &&
+
+    <>
+    <Product name="HP" price={60}/>
+    <Product name="Vivo" price={30}/>
+    <Product name="Acer" price={50}/>
+      </>
+   ||
+    <>
+    <Product name="Lenovo" price={75}/>
+    <Product name="Macbook" price={130}/>
+    </>
+   
+}
   </>
   );
 }//empty tag in react are called fragment
