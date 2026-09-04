accd445db83be9e894caf71faaa902143eb81579
Sanjana-53 <sanjanacps185@gmail.com>
2026-09-04 14:56:12 +0530

    update commit

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,17 +1,29 @@
 import Product from "./Product"
 const App = ()=>{
   const a=10;
+  let toggle = false;
+  let prodt;
+  if(toggle){
+
+    prodt = <>
+    <Product name="HP" price={60}/>
+    <Product name="Vivo" price={30}/>
+    <Product name="Acer" price={50}/>
+      </>
+   }
+   else{
+    prodt = <>
+    <Product name="Lenovo" price={75}/>
+    <Product name="Macbook" price={130}/>
+    </>
+   }
   return (
   <>                
     <h1>Hello World </h1>
     <h2>Hi Everyone</h2>
     <h3>{a}</h3>
     <h1>Product page</h1>
-    <Product name="HP" price={60}/>
-    <Product name="Vivo" price={30}/>
-    <Product name="Acer" price={50}/>
-    <Product name="Lenovo" price={75}/>
-    <Product name="Macbook" price={130}/>
+    {prodt}
   </>
   );
 }//empty tag in react are called fragment
--- demo/src/Product.jsx
+++ demo/src/Product.jsx
@@ -10,5 +10,4 @@ const Product = (props) =>{
     )
 }
 
-
 export default Product;
