d8f94a46b94aef7096b2693b85fcc817ef421463
Sanjana-53 <sanjanacps185@gmail.com>
2026-09-09 14:25:24 +0530

    update commit

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,34 +1,49 @@
-import Product from "./Product"
-const App = ()=>{
-  const a=10;
-  let toggle = false;
-  //let prodt;
+// import Product from "./Product"
+// const App = ()=>{
+//   const a=10;
+//   let toggle = false;
+//   //let prodt;
   
-  return (
-  <>                
-    <h1>Hello World </h1>
-    <h2>Hi Everyone</h2>
-    <h3>{a}</h3>
-    <h1>Product page</h1>
-    {
-      toggle &&
+//   return (
+//   <>                
+//     <h1>Hello World </h1>
+//     <h2>Hi Everyone</h2>
+//     <h3>{a}</h3>
+//     <h1>Product page</h1>
+//     {
+//       toggle &&
 
-    <>
-    <Product name="HP" price={60}/>
-    <Product name="Vivo" price={30}/>
-    <Product name="Acer" price={50}/>
-      </>
-   ||
-    <>
-    <Product name="Lenovo" price={75}/>
-    <Product name="Macbook" price={130}/>
-    </>
+//     <>
+//     <Product name="HP" price={60}/>
+//     <Product name="Vivo" price={30}/>
+//     <Product name="Acer" price={50}/>
+//       </>
+//    ||
+//     <>
+//     <Product name="Lenovo" price={75}/>
+//     <Product name="Macbook" price={130}/>
+//     </>
    
+// }
+//   </>
+//   );
+// }//empty tag in react are called fragment
+// export default App
+
+// //UI
+// //1. Imperative   2. Declarative
+
+
+
+import Card from "./Card"
+
+const App=()=>{
+  return(
+    <>
+    
+    <Card/>
+    </>
+  )
 }
-  </>
-  );
-}//empty tag in react are called fragment
 export default App
 
-//UI
-//1. Imperative   2. Declarative
