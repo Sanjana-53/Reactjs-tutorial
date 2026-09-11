3291a0c99f574a5158018e8f664b24ad38a4d3a0
Sanjana-53 <sanjanacps185@gmail.com>
2026-09-11 14:21:14 +0530

    update commit

--- demo/src/Card.jsx
+++ demo/src/Card.jsx
@@ -1,5 +1,23 @@
-const Card = ()=>{
-    return <h1>Card Component</h1>
-    
+import "./card.css"
+const Card = () => {
+    return (
+        <>
+            <div className="container">
+                <h1>Card Components</h1>
+                <div className="card">
+                    <div>
+                        <img className="img"
+                        src="https://static.vecteezy.com/system/resources/thumbnails/060/005/109/small/dramatic-chess-match-scene-intense-closeup-dark-background-copyspace-cinematic-atmosphere-free-photo.jpeg" 
+                        alt="Image-components" />
+                    </div>
+                    <div>
+                        <div className="title">Chess Board</div>
+                        <div className="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</div>
+                    </div>
+                    <button className="btn">Click Me</button>
+                </div>
+            </div>
+        </>
+    )
 }
-export default Card
+export default Card;
